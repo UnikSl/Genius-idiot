77d57124e225863cd47248f5b7ca7f0228a6eb99
unik <azesem@yandex.ru>
2026-09-16 08:42:34 +0300

    мелкие исправления ( чуток чистки кода )

--- Genius-IdiotConsoleApp/Program.cs
+++ Genius-IdiotConsoleApp/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace GeniusIdiotConsoleApp
+﻿namespace GeniusIdiotConsoleApp
 {
     internal class Program
-    {               
-        static void Main(string[] args)
-        {            
-            List<StudentResult> students = new List<StudentResult>();
-            List<Question> questions = new List<Question>();
-            List<Diagnosis> diagnoses = new List<Diagnosis>();
+    {
+        private static void Main(string[] args)
+        {
+            List<StudentResult> students = [];
+            List<Question> questions = [];
+            List<Diagnosis> diagnoses = [];
             FillQuestions(questions);
             FillDiagnoses(diagnoses);
             bool isRunning = true;
@@ -41,11 +38,11 @@ namespace GeniusIdiotConsoleApp
                     default:
                         Console.WriteLine("Неверный выбор.");
                         break;
-                }                
-            }                                    
+                }
+            }
         }
-                
-        static void TeacherLogin(List<StudentResult> students, List<Question> questions)
+
+        private static void TeacherLogin(List<StudentResult> students, List<Question> questions)
         {
             Console.WriteLine("Чтобы войти в Режим преподавателя, введите пароль: ");
             string password = Console.ReadLine();
@@ -60,8 +57,8 @@ namespace GeniusIdiotConsoleApp
                 return;
             }
         }
-        
-        static void TeacherMenu(List<StudentResult> students, List<Question> questions)
+
+        private static void TeacherMenu(List<StudentResult> students, List<Question> questions)
         {
             bool isRunning = true;
             while (isRunning)
@@ -92,8 +89,8 @@ namespace GeniusIdiotConsoleApp
                 }
             }
         }
-        
-        static void ShowGroupStatistics(List<StudentResult> students)
+
+        private static void ShowGroupStatistics(List<StudentResult> students)
         {
             Console.WriteLine("Статистика группы:");
             foreach (var student in students)
@@ -101,14 +98,14 @@ namespace GeniusIdiotConsoleApp
                 Console.WriteLine($"Имя: {student.StudentName}, Правильные ответы: {student.CorrectAnswers}, Диагноз: {student.Diagnosis}");
             }
         }
-                
+
         public class StudentResult
         {
             public string StudentName { get; set; }
             public int CorrectAnswers { get; set; }
             public string Diagnosis { get; set; }
         }
-                
+
         public class Question
         {
             public string Text { get; set; }
@@ -118,28 +115,28 @@ namespace GeniusIdiotConsoleApp
         public class Diagnosis
         {
             public string DiagnosisName { get; set; }
-            public int DiagnosisCode { get; set; }            
+            public int DiagnosisCode { get; set; }
         }
 
-        static void ShowQuestionsAnswers(List<Question> questions)
+        private static void ShowQuestionsAnswers(List<Question> questions)
         {
             foreach (var question in questions)
             {
                 Console.WriteLine($"Вопрос: {question.Text}, Ответ: {question.Answer}");
             }
         }
-                
-        static void Test(List<StudentResult> students, List<Question> questions, List<Diagnosis> diagnoses)
-        {               
-            List<Question> questionsList = new List<Question>(questions);
+
+        private static void Test(List<StudentResult> students, List<Question> questions, List<Diagnosis> diagnoses)
+        {
+            List<Question> questionsList = [.. questions];
             Console.WriteLine("Добрый день. Введите свое имя...");
             string studentName = Console.ReadLine();
             int i = 1;
-            int correctAnswers = 0; 
+            int correctAnswers = 0;
             while (questionsList.Count > 0)
             {
-                
-                int randomIndex = Random.Shared.Next(questionsList.Count); 
+
+                int randomIndex = Random.Shared.Next(questionsList.Count);
                 Console.Write($"Вопрос № {i}: ");
                 Console.WriteLine(questionsList[randomIndex].Text);
                 int answer = GetValidInput();
@@ -147,7 +144,7 @@ namespace GeniusIdiotConsoleApp
                 if (answer == questionsList[randomIndex].Answer)
                 {
                     correctAnswers++;
-                }                
+                }
                 questionsList.RemoveAt(randomIndex);
                 i++;
             }
@@ -160,21 +157,21 @@ namespace GeniusIdiotConsoleApp
                     break;
                 }
             }
-            students.Add(new StudentResult { StudentName = studentName, CorrectAnswers = correctAnswers, Diagnosis = diagnosisName });            
-            
+            students.Add(new StudentResult { StudentName = studentName, CorrectAnswers = correctAnswers, Diagnosis = diagnosisName });
+
             Console.WriteLine(studentName + ", ваш диагноз: " + diagnosisName);
         }
-        
-        static void FillQuestions (List<Question> questions)
+
+        private static void FillQuestions(List<Question> questions)
         {
             questions.Add(new Question { Text = "Сколько будет 2 плюс 2, умноженное на 2?", Answer = 6 });
             questions.Add(new Question { Text = "Бревно нужно распилить на 10 частей. Сколько надо сделать распилов?", Answer = 9 });
             questions.Add(new Question { Text = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?", Answer = 25 });
             questions.Add(new Question { Text = "Укол делают каждые полчаса. Сколько нужно минут для трех уколов?", Answer = 60 });
-            questions.Add(new Question { Text = "5 свечей горело, 2 потухли. Сколько свечей осталось?", Answer = 2 });            
+            questions.Add(new Question { Text = "5 свечей горело, 2 потухли. Сколько свечей осталось?", Answer = 2 });
         }
-                
-        static void FillDiagnoses(List<Diagnosis> diagnoses)
+
+        private static void FillDiagnoses(List<Diagnosis> diagnoses)
         {
             diagnoses.Add(new Diagnosis { DiagnosisName = "Гений", DiagnosisCode = 5 });
             diagnoses.Add(new Diagnosis { DiagnosisName = "Идиот", DiagnosisCode = 0 });
@@ -182,11 +179,11 @@ namespace GeniusIdiotConsoleApp
             diagnoses.Add(new Diagnosis { DiagnosisName = "Талант", DiagnosisCode = 4 });
             diagnoses.Add(new Diagnosis { DiagnosisName = "Дурак", DiagnosisCode = 2 });
             diagnoses.Add(new Diagnosis { DiagnosisName = "Нормальный", DiagnosisCode = 3 });
-            
-            
+
+
         }
-        static int GetValidInput()
-        {            
+        private static int GetValidInput()
+        {
             int answer = 0;
             while (true)
             {
@@ -196,7 +193,7 @@ namespace GeniusIdiotConsoleApp
                     Console.WriteLine($"Ваш ответ {answer}");
                     break;
                 }
-                Console.WriteLine("Вы ввели не число");                
+                Console.WriteLine("Вы ввели не число");
             }
             return answer;
         }
